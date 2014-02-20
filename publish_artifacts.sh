9bedd92f4a05e7b4bb538424ce5918c0c7cd57ff
Ilya Murzinov <ilya-murzinov@users.noreply.github.com>
2014-02-20 21:30:25 +0400

    trying to deploy

---
+++ publish_artifacts.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo -e "Publishing artifacts...\n"
+
+cp -R build/docs/javadoc $HOME/javadoc-latest
+
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/4242/delmgorb gh-pages > /dev/null
+
+cd gh-pages
+git rm -rf ./artifacts
+cp -Rf $HOME/target ./artifacts
+git add -f ./delmgorb-1.0.jar
+git commit -m "Lastest artifacts on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Published artifacts to gh-pages.\n"
