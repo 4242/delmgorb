31d45822f812d10df2b8637328874119a8b8a981
travis-ci <travis@travis-ci.org>
2014-02-20 22:06:58 +0400

    trying to deploy

--- publish_artifacts.sh
+++ publish_artifacts.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 echo -e "Publishing artifacts...\n"
-
-cp -R ./target $HOME/target
+cp -R ./target $HOME/artifacts
 
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/4242/delmgorb gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GH-TOKEN}@github.com/4242/delmgorb gh-pages > /dev/null
 
 cd gh-pages
 git rm -rf ./artifacts
-cp -Rf $HOME/target ./artifacts
-git add -f ./delmgorb-1.0.jar
+cp -Rf $HOME/artifacts ./gh-pages/artifacts
+git add -f ./artifacts/delmgorb-1.0.jar
 git commit -m "Lastest artifacts on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
 
