314fc0661653cb2243c574a519f3d81574790d0c
ilya-murzinov <murz42@gmail.com>
2014-02-21 10:00:42 -0800

    added new plot using jzy3d

--- src/main/java/com/organization4242/delmgorb/Application/Application.java
+++ src/main/java/com/organization4242/delmgorb/Application/Application.java
@@ -3,14 +3,19 @@ package com.organization4242.delmgorb.Application;
 import com.organization4242.delmgorb.Controller.GraphController;
 import com.organization4242.delmgorb.Model.GraphModel;
 import com.organization4242.delmgorb.View.GraphView;
+import com.organization4242.delmgorb.View.SurfaceDemo;
+import org.jzy3d.analysis.AnalysisLauncher;
 
 /**
  * Created by ilya-murzinov on 19.02.14.
  */
 public class Application {
-    public static void main(String[] args) {
-        GraphView view = new GraphView();
-        new GraphController(new GraphModel(), view);
-        view.display();
+//    public static void main(String[] args) {
+//        GraphView view = new GraphView();
+//        new GraphController(new GraphModel(), view);
+//        view.display();
+//    }
+    public static void main(String[] args) throws Exception {
+        AnalysisLauncher.open(new SurfaceDemo());
     }
 }
