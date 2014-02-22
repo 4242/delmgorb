6b0951bce15646d0ef09384a387290b5d7a46373
ilya-murzinov <murz42@gmail.com>
2014-02-22 14:48:49 +0400

    changed plotting library, added new sample

--- src/main/java/com/organization4242/delmgorb/Application/Application.java
+++ src/main/java/com/organization4242/delmgorb/Application/Application.java
@@ -1,11 +1,5 @@
 package com.organization4242.delmgorb.Application;
 
-import com.organization4242.delmgorb.Controller.GraphController;
-import com.organization4242.delmgorb.Model.GraphModel;
-import com.organization4242.delmgorb.View.GraphView;
-import com.organization4242.delmgorb.View.SurfaceDemo;
-import org.jzy3d.analysis.AnalysisLauncher;
-
 /**
  * Created by ilya-murzinov on 19.02.14.
  */
@@ -16,6 +10,7 @@ public class Application {
 //        view.display();
 //    }
     public static void main(String[] args) throws Exception {
-        AnalysisLauncher.open(new SurfaceDemo());
+
+        //AnalysisLauncher.open(new SurfaceDemo());
     }
 }
