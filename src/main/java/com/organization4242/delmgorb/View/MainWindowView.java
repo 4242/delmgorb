b91db21a657a231e380a85f248cd84560d73dcd0
ilya-murzinov <murz42@gmail.com>
2014-02-24 00:33:44 +0400

    added combobox to main window

--- src/main/java/com/organization4242/delmgorb/View/MainWindowView.java
+++ src/main/java/com/organization4242/delmgorb/View/MainWindowView.java
@@ -11,6 +11,7 @@ public class MainWindowView extends JPanel{
     private JLabel label;
     private int textFieldNumber = 14;
     private JTextField[] textFields = new JTextField[textFieldNumber];
+    private JComboBox<IntegrationMethods> comboBox;
     private JButton button;
 
     public JTextField[] getTextFields() {
@@ -38,6 +39,9 @@ public class MainWindowView extends JPanel{
             textFields[i].setText("0");
         }
         button = new JButton("Draw!");
+        comboBox = new JComboBox<IntegrationMethods>(IntegrationMethods.values());
+        comboBox.setEditable(false);
+        //comboBox.
         GridBagLayout gbl = new GridBagLayout();
         jf.setLayout(gbl);
         GridBagConstraints c =  new GridBagConstraints();
@@ -65,6 +69,10 @@ public class MainWindowView extends JPanel{
             jf.add(textFields[i]);
         }
         c.gridy++;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        gbl.setConstraints(comboBox, c);
+        jf.add(comboBox);
         c.gridx = 5;
         c.gridwidth = 2;
         c.insets = new Insets(10, 0, 0, 0);
@@ -75,4 +83,19 @@ public class MainWindowView extends JPanel{
     public void display() {
         jf.setVisible(true);
     }
+}
+
+enum IntegrationMethods {
+    Euler,
+    Midpoint,
+    ClassicalRungeKutta,
+    Gill,
+    ThreeEights,
+    HighamAndHall,
+    DormandPrince5,
+    DormandPrince8,
+    GraggBulirschStoer,
+    AdamsBashforth,
+    AdamsMoulton,
+
 }
