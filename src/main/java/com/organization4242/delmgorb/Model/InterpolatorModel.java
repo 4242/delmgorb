519a52b8e6d7be8695658bb7159bbce30d856d40
ilya-murzinov <murz42@gmail.com>
2014-02-26 20:44:48 +0400

    changed interpolation method

--- src/main/java/com/organization4242/delmgorb/Model/InterpolatorModel.java
+++ src/main/java/com/organization4242/delmgorb/Model/InterpolatorModel.java
@@ -1,13 +1,13 @@
 package com.organization4242.delmgorb.Model;
 
 import org.apache.commons.math3.analysis.interpolation.BicubicSplineInterpolatingFunction;
-import org.apache.commons.math3.analysis.interpolation.BicubicSplineInterpolator;
+import org.apache.commons.math3.analysis.interpolation.SmoothingPolynomialBicubicSplineInterpolator;
 
 /**
  * Created by ilya-murzinov on 24.02.14.
  */
 public class InterpolatorModel {
-    BicubicSplineInterpolator interpolator = new BicubicSplineInterpolator();
+    SmoothingPolynomialBicubicSplineInterpolator interpolator = new SmoothingPolynomialBicubicSplineInterpolator();
 
     public BicubicSplineInterpolatingFunction getFunction(ThreeArrays arrays) {
         return interpolator.interpolate(arrays.x_val, arrays.y_val, arrays.f_val);
