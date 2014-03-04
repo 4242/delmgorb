b913712e094b399a1824a48b0f425643ad535ba2
ilya-murzinov <murz42@gmail.com>
2014-03-05 01:51:41 +0400

    fixed IntegrationMethods naming

--- src/main/java/com/organization4242/delmgorb/model/IntegrationMethods.java
+++ src/main/java/com/organization4242/delmgorb/model/IntegrationMethods.java
@@ -4,16 +4,26 @@ package com.organization4242.delmgorb.model;
  * Created by ilya-murzinov on 24.02.14.
  */
 public enum IntegrationMethods {
-    EULER,
-    MIDPOINT,
-    CLASSICAL_RUNGE_KUTTA,
-    GILL,
-    THREE_EIGHTS,
-    HIGHAM_AND_HALL,
-    DORMAND_PRINCE_5,
-    DORMAND_PRINCE_8,
-    GRAGG_BULIRSCH_STOER,
-    ADAMS_BASHFORTH,
-    ADAMS_MOULTON,
+    EULER("Euler"),
+    MIDPOINT("Midpoint"),
+    CLASSICAL_RUNGE_KUTTA("Classical Runge-Kutta"),
+    GILL("Gill"),
+    THREE_EIGHTS("Three eights"),
+    HIGHAM_AND_HALL("Higham and Hall"),
+    DORMAND_PRINCE_5("Dormand-Prince 5"),
+    DORMAND_PRINCE_8("Dormand-Prince 5"),
+    GRAGG_BULIRSCH_STOER("Grass-Bulirch-Stoer"),
+    ADAMS_BASHFORTH("Adams-Bashforth"),
+    ADAMS_MOULTON("Adams-Moulton");
 
+    private final String name;
+
+    private IntegrationMethods(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
